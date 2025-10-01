719e36b4651f27bf661660545cdbbaf1d7bce139
luka khokhashvili <119745295+Luka-khokhashvili@users.noreply.github.com>
2025-10-01 20:12:56 +0400

    feat: implement electronics subclass that extends product class

---
+++ src/productModels/electronics.ts
@@ -0,0 +1,20 @@
+import { product } from './product';
+
+export class name extends product {
+  private _warrantyYears: number;
+
+  constructor(id: number, name: string, price: number, warrantyYears: number) {
+    super(id, name, price);
+    this._warrantyYears = warrantyYears;
+  }
+
+  public get warrantyYears(): number {
+    return this._warrantyYears;
+  }
+
+  // Electronics mostly get smaller discount on their prices
+  public override getDiscountedPrice(discount: number): number {
+    const effectiveDiscount = Math.min(discount * 0.05);
+    return super.getDiscountedPrice(effectiveDiscount);
+  }
+}
